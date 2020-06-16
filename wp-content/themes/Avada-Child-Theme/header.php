1c32c92c3069a6856b6d27072ced20bca8204e64
Mitchlee <mitch.lambie@nelnet.net>
2020-06-16 15:02:51 -0500

    PROD and DEV GTM code for OneTrust

--- wp-content/themes/Avada-Child-Theme/header.php
+++ wp-content/themes/Avada-Child-Theme/header.php
@@ -15,13 +15,28 @@ if ( ! defined( 'ABSPATH' ) ) {
 <?php global $woocommerce; ?>
 <html class="<?php echo ( Avada()->settings->get( 'smooth_scrolling' ) ) ? 'no-overflow-y' : ''; ?>" <?php language_attributes(); ?>>
 <head>
-	<!-- Google Tag Manager -->
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-WSMR9DD');</script>
-	<!-- End Google Tag Manager -->
+  <?php if (!is_user_logged_in()) { ?>
+
+    <?php
+    if (home_url() == 'https://paymentspring.com') { ?>
+      <!-- Google Tag Manager PROD -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WSMR9DD');</script>
+      <!-- End Google Tag Manager PROD -->
+    <?php } else { ?>
+      <!-- Google Tag Manager DEV -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T9M2HWP');</script>
+      <!-- End Google Tag Manager DEV -->
+    <?php } ?>
+  <?php } ?>
+
 
 	<script>!function(s,a,e,v,n,t,z){if(s.saq)return;n=s.saq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!s._saq)s._saq=n;
 n.push=n;n.loaded=!0;n.version='1.0';n.queue=[];t=a.createElement(e);t.async=0;t.src=v;z=a.getElementsByTagName(e)[0];z.parentNode.insertBefore(t,z)}(window,document,'script','https://tags.srv.stackadapt.com/events.js');saq('ts', '4aigcwfZ6MBYG_ndl-A_Qg');</script>
@@ -113,10 +128,23 @@ if ( 'modern' === Avada()->settings->get( 'mobile_menu_design' ) ) {
 
 ?>
 <body <?php body_class(); ?>>
-	<!-- Google Tag Manager (noscript) -->
-		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WSMR9DD"
-		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-	<!-- End Google Tag Manager (noscript) -->
+
+<?php if (!is_user_logged_in()) { ?>
+
+  <?php
+  if (home_url() == 'https://factsmgt.com') { ?>
+    <!-- Google Tag Manager (noscript) PROD -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WSMR9DD"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) PROD -->
+  <?php } else { ?>
+    <!-- Google Tag Manager (noscript) DEV -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T9M2HWP"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) DEV -->
+  <?php } ?>
+<?php } ?>
+
 
 	<?php do_action( 'avada_before_body_content' );
 
